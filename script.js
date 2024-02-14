540b988c37c91817976c9b358dada087b52464fe
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-02-14 13:07:56 +0100

    Börjat med javascript functioner för min kundvagn och mina knappar för produkterna

---
+++ script.js
@@ -0,0 +1,23 @@
+function lägg_blue_i_vagn() {
+
+}
+
+function lägg_grey_i_vagn() {
+
+}
+
+function lägg_pink_i_vagn() {
+    
+}
+
+function lägg_white_i_vagn() {
+    
+}
+
+function lägg_red_i_vagn() {
+    
+}
+
+function lägg_goof_i_vagn() {
+    
+}
